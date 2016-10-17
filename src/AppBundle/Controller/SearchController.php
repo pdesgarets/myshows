481406276638006a738ac86589eceb0824d70ee5
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-10-17 11:30:26 +0200

    Make actual search work

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -5,18 +5,25 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class SearchController
+ * @Route("/search")
+ */
 class SearchController extends Controller
 {
     /**
-     * @Route("/results")
+     * @Route("/results", name="search_results")
      * @Template("/search/results.html.twig")
+     * @param Request $request
+     * @return array
      */
-    public function resultsAction()
+    public function resultsAction(Request $request)
     {
-        $shows = $this->get('myshows.fetcher.shows')->getShows('friends');
+        $query = $request->get('q');
+        $shows = $this->get('myshows.fetcher.shows')->getShows($query);
 
-        // replace this example code with whatever you need
         return array('shows' => $shows);
     }
 
