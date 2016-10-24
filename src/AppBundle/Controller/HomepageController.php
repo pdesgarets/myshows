324a6098b9b225f7e6d1e62722fb25245f5da194
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-10-24 12:20:41 +0200

    add alerts on homepage

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\TVShow;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -16,8 +17,33 @@ class HomepageController extends Controller
     public function indexAction()
     {
         $shows = $this->getUser()->getShows();
+        $this->setAlertsForComingShows();
 
         // replace this example code with whatever you need
         return array('shows' => $shows);
+    }
+
+    private function setAlertsForComingShows()
+    {
+        $shows = $this->getUser()->getShows();
+        $timing = new \DateInterval($this->getParameter('timing_alert'));
+        $now = new \DateTime();
+        $limit = (new \DateTime())->add($timing);
+        /** @var TVShow $show */
+        foreach ($shows as $show) {
+            foreach ($this->get('myshows.fetcher.shows')->getEpisodes($show->getId()) as $episode) {
+                $airDate = new \DateTime($episode->airstamp);
+                $diff = $airDate->diff($now);
+                if ($airDate->getTimestamp() > $now->getTimestamp() && $airDate->getTimestamp() < $limit->getTimestamp()) {
+                    $this->addFlash(
+                        'warning',
+                        'Un nouvel épisode de <a href="' .
+                        $this->generateUrl('tv_show_display', array('id' => $show->getId())) . '">' . $show->getName() .
+                        '</a> sera disponible dans ' . ((int) $diff->format('%d') > 0 ? $diff->format('%d jours') : $diff->format('%h heures')) . ' !'
+                    );
+                    break;
+                }
+            }
+        }
     }
 }
