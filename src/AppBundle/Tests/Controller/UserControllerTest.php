03156a66f8f2fbf782a079d8b28ce48d8b87620a
josue <josue.lengronne@student.ecp.fr>
2016-10-25 15:56:36 +0200

    [test] UserControllerTest : test complete and valid

--- src/AppBundle/Tests/Controller/UserControllerTest.php
+++ src/AppBundle/Tests/Controller/UserControllerTest.php
@@ -9,7 +9,7 @@ class UserControllerTest extends WebTestCase
     
     public function testCompleteScenario()
     {
-        /*// Create a new client to browse the application
+        // Create a new client to browse the application
         $client = static::createClient(array(), array(
             'PHP_AUTH_USER' => 'god',
             'PHP_AUTH_PW' => 'password'
@@ -22,21 +22,25 @@ class UserControllerTest extends WebTestCase
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_user[field_name]'  => 'Test',
+            'user[email]'  => 'john@cagnol.com',
+            'user[username]'  => 'johnc',
+            'user[plainPassword][first]'  => 'password',
+            'user[plainPassword][second]' => 'password',
             // ... other fields to fill
         ));
+        $form['user[roles]'][0]->tick();
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("johnc")')->count(), 'Missing element td:contains("johnc")');
 
         // Edit the entity
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_user[field_name]'  => 'Foo',
+        $form = $crawler->selectButton('Edit')->form(array(
+            'user[username]'  => 'johnk',
             // ... other fields to fill
         ));
 
@@ -44,14 +48,14 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+        $this->assertGreaterThan(0, $crawler->filter('[value="johnk"]')->count(), 'Missing element [value="johnk"]');
 
         // Delete the entity
         $client->submit($crawler->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());*/
+        $this->assertNotRegExp('/johnk/', $client->getResponse()->getContent());
     }
 
 }
